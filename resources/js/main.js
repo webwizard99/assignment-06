697f106480e0efef72ca77ce2e516ff2a782df62
Michael Bragg <michael_bragg99@hotmail.com>
2019-11-04 13:36:09 -0700

    Move main.js page load code from onload event to global scope.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,20 +1,19 @@
 import storeitems from './storeitems.js';
 
-document.addEventListener('onload', () => {
-  storeitems.initTestItems(40);
-  const items = storeitems.getItems();
-  console.log(items);
-  const storeFrame = document.querySelector('.store-frame');
-  items.forEach((item, itemN) => {
-    storeFrame.innerHTML += `
-    <div class="store-item" key="${itemN}">
-        <div class="item-pic" style={ background-image: ${item.img}}>
-          <p class="reader">${item.alt}</p>
-        </div>
-        <p class="item-title">${item.name}</p>
-        <p class="item-description">Scented Epsom Salt</p>
-        <p class="item-price">${item.price}</p>
+
+storeitems.initTestItems(40);
+const items = storeitems.getItems();
+console.log(items);
+const storeFrame = document.querySelector('.store-frame');
+items.forEach((item, itemN) => {
+  storeFrame.innerHTML += `
+  <div class="store-item" key="${itemN}">
+      <div class="item-pic" style={ background-image: ${item.img}}>
+        <p class="reader">${item.alt}</p>
       </div>
-    `
-  });
+      <p class="item-title">${item.name}</p>
+      <p class="item-description">Scented Epsom Salt</p>
+      <p class="item-price">${item.price}</p>
+    </div>
+  `
 });
